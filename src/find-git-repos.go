146a9c5670ee7dbea8a15c1de234e206e7a00c74
Shiva <shiv@shiv.me>
2014-05-28 22:36:07 -0700

    find-git-repos: A tool to look for git repos in hierarchy

---
+++ src/find-git-repos.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "path/filepath"
+    "os"
+    "flag"
+    "fmt"
+)
+
+func visit(path string, f os.fileinfo, err error) error {
+    fmt.Printf("Visited: %s\n", path)
+    return nil
+}
+
+func main() {
+    flag.Parse()
+    root := flag.Arg(0)
+    err := filepath.Walk(root, visit)
+    fmt.Printf("filepath.Walk() returned %v\n", err)
+}
